e3839375da392f37702f927ff996d4029861b20d
G&D Iberica <gdiberica@Demos-MacBook-Air.local>
2017-10-03 11:10:09 +0200

    Initial Commit

---
+++ Prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by G&D Iberica on 03/10/2017.
+//  Copyright © 2017 G&D Iberica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
